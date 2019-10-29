9eeb0006dc0cea9c3a2a148ea58fb2663ce5011e
taesiri <mtaesiri@gmail.com>
2019-10-29 05:22:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "colabstraemer.py",
     version = "0.1",
-    py_modules = ['colbastreamer'],
+    py_modules = ['colabstraemer'],
     url = "https://github.com/taesiri/colabstraemer",
     author = "taesiri",
 )
